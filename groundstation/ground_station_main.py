fe91231d9f9c1bb14aaf8bc5a5502ac6c90d7f2c
Shenhavo <45486101+Shenhavo@users.noreply.github.com>
2020-12-05 13:30:08 +0200

    Fps value is added to pyplot

--- groundstation/ground_station_main.py
+++ groundstation/ground_station_main.py
@@ -12,7 +12,6 @@ import constants as c
 import matplotlib.pyplot as plt
 
 
-
 def main():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -52,6 +51,7 @@ def main():
 
     pm = PacketMngr(c.HOST, c.PORT)
     pm.csvmngr = csvmngr
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         pm.add_socket(s)
         pm.socket.connect((pm.host, pm.port))
@@ -90,6 +90,9 @@ def main():
                         expected_frame_size = pm.frame_size - c.FRAME_HEADER_SIZE_B
                         print("bad recv size: %d" % expected_frame_size)
                     else:
+                        pm.frame_ctr = pm.frame_ctr + 1
+                        if pm.first_img_tick == 0:
+                            pm.first_img_tick = incoming_sys_tick
                         print("frame recv size: %d" % len(pm.frame_rx_buff))
                         pm.save_jpeg(path)
                         pm.calc_ber()
@@ -97,6 +100,9 @@ def main():
                         delta_tick = pm.frame_sys_tick - pm.last_valid_frame_sys_tick
                         print("sys_tick diff = %d [msec]" % delta_tick)
                         pm.last_valid_frame_sys_tick = pm.frame_sys_tick
+                        pm.curr_img_tick = pm.frame_sys_tick
+
+
                     # ===== cleaning:  ======
                     pm.clear_frame_properties()
 
@@ -119,9 +125,6 @@ def main():
             input()
             exit(1)
 
-
-
-
 class PacketMngr:
 
     def __init__(self, a_host, a_port):
@@ -131,11 +134,14 @@ class PacketMngr:
         self.rx_bytes_to_read = 1
         self.frame_rx_buff = bytes()
         self.frame_sys_tick = 0
+        self.first_img_tick = 0.0
+        self.curr_img_tick = 0.0
         self.last_valid_frame_sys_tick = 0
         self.frame_size =   0
         self.ptr_jpeg   = None
         self.last_saved_img_path = ''
         self.last_saved_cropped_img_path = ''
+        self.frame_ctr = 0
 
         self.imu_rx_buff = bytes()
         self.imu_sys_tick = 0
@@ -173,6 +179,8 @@ class PacketMngr:
 
     def display_img(self):
         img_plt = None
+        text = None
+
         while True:
             if self.new_img_rec:
                 self.new_img_rec = False
@@ -181,8 +189,13 @@ class PacketMngr:
                     img = plt.imread(self.last_saved_cropped_img_path)
                     if img_plt is None:
                         img_plt = plt.imshow(img)
+                        text = plt.text(0, 0, "hola", bbox=dict(fill=False, edgecolor='red', linewidth=2))
                     else:
                         img_plt.set_data(img)
+                        dt = (self.curr_img_tick-self.first_img_tick)
+                        if dt != 0:
+                            s = 'Average%.3f[fps]' %(self.frame_ctr*1000/dt)
+                            text.set_text(s)
                     plt.pause(c.IMG_PLOT_PAUSE)  # needs to be less then 1/15fps
                     plt.draw()
                 except:
@@ -229,7 +242,6 @@ class PacketMngr:
         self.packet_ctr =   0
         self.frame_sys_tick   =   0
         self.frame_size =   0
-        self.frame_ctr  =   0
         self.expected_num_of_packets = 0
         self.ptr_jpeg   = None
         self.mode       =   0 # 0 = off, 1 = Image, 2 = IMU
@@ -256,6 +268,7 @@ class PacketMngr:
             self.acc_y = TwoBytesToInt16(a_byte_arr,c.IMU_DATA_SHIFT_SIZE_B + imu_call_idx * c.IMU_CALL_SIZE_B + c.ACC_Y_SHIFT) / c.ACC_BITS_TO_FLOAT
             self.acc_z = TwoBytesToInt16(a_byte_arr,c.IMU_DATA_SHIFT_SIZE_B + imu_call_idx * c.IMU_CALL_SIZE_B + c.ACC_Z_SHIFT) / c.ACC_BITS_TO_FLOAT
             self.csvmngr.appendImuData([self.imu_sys_tick-c.IMU_CALLS_TIME_Delta_MSEC*(c.IMU_CALLS_PER_PACKET-1-imu_call_idx),self.gyro_x,self.gyro_y,self.gyro_z,self.acc_x,self.acc_y,self.acc_z])
+
     def print_imu_data(self):
         prnt(("Gyro X = " + str(self.gyro_x)), 0)
         prnt(("Gyro Y = " + str(self.gyro_y)), 0)
